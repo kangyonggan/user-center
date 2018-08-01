7959591464bfb962550355d546a33426b52164ea
kangyonggan <kangyonggan@gmail.com>
2018-08-01 11:01:09 +0800

    MapperScan

--- uc-service/src/main/java/com/kangyonggan/uc/Application.java
+++ uc-service/src/main/java/com/kangyonggan/uc/Application.java
@@ -1,6 +1,5 @@
 package com.kangyonggan.uc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 7/31/18
  */
 @SpringBootApplication
-@MapperScan("com.kangyonggan.uc.mapper.*")
 public class Application {
 
     public static void main(String[] args) {
